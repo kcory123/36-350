2e8f88558cc87c9efd4ff572fd735607b8494e85
kcory123 <kcory@andrew.cmu.edu>
2018-04-19 12:16:21 -0400

    fits the linear regression responses ~ covariates and retains only those covariates whose coefficient p-values are less than or equal to cutoff. Then fit another regression using only the retained covariates and return the p-values from this reduced model.

--- lab_12_simulation_final.R
+++ lab_12_simulation_final.R
@@ -1,3 +1,4 @@
+
 generate_data = function(n,p) {
   covariates= matrix( rnorm(n*p,0,1), nrow = n, ncol = p)
   responses = vector(length = n)
@@ -5,4 +6,12 @@ generate_data = function(n,p) {
     responses[i] = rnorm(1,0,1)
   }
   return(list(covariates = covariates, responses = responses))
+}
+
+model_select = function(covariates, responses, cutoff) {
+  regression = lm(responses~ covariates)
+  retained.covariates = which(regression$p.value <= cutoff)
+  regression2 = lm(responses ~ covariates[,retained.covariates])
+  return(regression2$p.value)
+  
 }
